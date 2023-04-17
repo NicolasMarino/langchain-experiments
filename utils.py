e4fd8b70bb01d3aeea4385a7773b813115f44e24
Nicolás Marino <nicolasmarino.17@gmail.com>
2023-04-17 20:23:34 -0300

    Testing langchain summary.

---
+++ utils.py
@@ -0,0 +1,11 @@
+import time
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Execution time for {func.__name__}: {execution_time:.6f} seconds")
+        return result
+    return wrapper
